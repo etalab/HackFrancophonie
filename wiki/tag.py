7f88f539efeb034c2039d2093e3b647bc6fcc0f7
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-01-21 10:45:51 +0100

    automatise la création des pages "thème"

---
+++ wiki/tag.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+@author: alexis
+
+Repère les pages qui contiennent une chaine de caractère données et
+créer une page (du nom de cette chaine de caractère) avec la liste des ces pages.
+
+# Le programme se déroule en deux temps:
+#  1 - lecture des fichiers qui contienne une chaine de caractère
+#  2 - écriture de la page destination
+"""
+import os
+
+path_wiki_files = "C:\git\HackFrancophonie.wiki" #replace with your path
+
+
+pages = os.listdir(path_wiki_files)
+# on regarde ce qui est une page et on retire les pages Accès...
+pages = [x for x in pages if x[-3:] == '.md' and 'Acc' not in x]
+pages.remove('_Sidebar.md')
+
+def list_pages_with_chain(chain):
+    list_pages_with_chain = []
+    for page in pages:
+        path_file = os.path.join(path_wiki_files, page)
+        text = open(path_file).read()
+        if '[[' + chain + ']]' in text:
+            list_pages_with_chain.append(page)
+    return list_pages_with_chain
+
+
+def write_list(chain, list_of_pages):
+    destination_path = os.path.join(path_wiki_files, chain.decode('utf8') + '.md')
+
+    text = ''
+    for page in list_of_pages:
+        page_text = '* [[' + page[:-3] + ']]\n'
+        text += page_text
+
+    with open(destination_path, 'w') as dest:
+        dest.write(text)
+
+
+for chain in ['Démocratie', 'Economie', 'Education', 'Sécurité', 'Société']:
+    write_list(chain, list_pages_with_chain(chain))
