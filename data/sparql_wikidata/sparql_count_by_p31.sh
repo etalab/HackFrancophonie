a4352ce8572aae4a33c4684160dcec8db5894046
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2016-02-12 19:05:33 +0100

    formatting csv + adding France

--- data/sparql_wikidata/sparql_count_by_p31.sh
+++ data/sparql_wikidata/sparql_count_by_p31.sh
@@ -57,7 +57,7 @@ WHERE
    }
 }
 GROUP BY ?nature_id ?nature_frlabel
-ORDER BY DESC(?count)' > wikidata_cote_d_ivoire_count_p31.csv
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_cote_d_ivoire_count_p31.csv
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query='PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
@@ -77,7 +77,7 @@ WHERE
    }
 }
 GROUP BY ?nature_id ?nature_frlabel
-ORDER BY DESC(?count)' > wikidata_haiti_count_p31.csv
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_haiti_count_p31.csv
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query='PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
@@ -97,7 +97,7 @@ WHERE
    }
 }
 GROUP BY ?nature_id ?nature_frlabel
-ORDER BY DESC(?count)' > wikidata_maurice_count_p31.csv
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_maurice_count_p31.csv
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query='PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
@@ -117,7 +117,7 @@ WHERE
    }
 }
 GROUP BY ?nature_id ?nature_frlabel
-ORDER BY DESC(?count)' > wikidata_mali_count_p31.csv
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_mali_count_p31.csv
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query='PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
@@ -137,4 +137,24 @@ WHERE
    }
 }
 GROUP BY ?nature_id ?nature_frlabel
-ORDER BY DESC(?count)' > wikidata_senegal_count_p31.csv
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_senegal_count_p31.csv
+
+curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query='PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX v: <http://www.wikidata.org/prop/statement/>
+PREFIX q: <http://www.wikidata.org/prop/qualifier/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?nature_id ?nature_frlabel (COUNT(distinct ?id) as ?count)
+WHERE
+{
+   ?id wdt:P17 wd:Q142 .
+   OPTIONAL {
+     ?id wdt:P31 ?nature_id .
+     ?nature_id rdfs:label ?nature_frlabel  filter (lang(?nature_frlabel ) = "fr") .
+   }
+}
+GROUP BY ?nature_id ?nature_frlabel
+ORDER BY DESC(?count)' -H 'Accept: text/csv' > wikidata_france_count_p31.csv
