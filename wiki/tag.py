cffe338b31cce7e75b9f184c2aa5b1a041d58414
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-02-16 10:22:39 +0100

    minor changes

--- wiki/tag.py
+++ wiki/tag.py
@@ -9,6 +9,7 @@ créer une page (du nom de cette chaine de caractère) avec la liste des ces pag
 #  1 - lecture des fichiers qui contienne une chaine de caractère
 #  2 - écriture de la page destination
 """
+
 import os
 
 path_wiki_files = "C:\git\HackFrancophonie.wiki" #replace with your path
@@ -19,11 +20,14 @@ pages = os.listdir(path_wiki_files)
 pages = [x for x in pages if x[-3:] == '.md' and 'Acc' not in x]
 pages.remove('_Sidebar.md')
 
+
 def list_pages_with_chain(chain):
     list_pages_with_chain = []
     for page in pages:
+        #ouvre le fichier
         path_file = os.path.join(path_wiki_files, page)
         text = open(path_file).read()
+        # regarde s'il y a le tag
         if '[[' + chain + ']]' in text:
             list_pages_with_chain.append(page)
     return list_pages_with_chain
@@ -41,5 +45,8 @@ def write_list(chain, list_of_pages):
         dest.write(text)
 
 
-for chain in ['Démocratie', 'Economie', 'Education', 'Sécurité', 'Société']:
+
+# ['Démocratie', 'Economie', 'Education', 'Sécurité', 'Société', 'Mali']
+for chain in ['Cartographie', 'Démocratie', 'Economie', 'Education',
+              'Justice', 'Sécurité', 'Population', 'Santé']:
     write_list(chain, list_pages_with_chain(chain))
